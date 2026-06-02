2dfbc6b727a26402231a4f6dbde2ae8d763fa6ba
vo phu hao <vophuhao2004@gmail.com>
2026-06-02 14:24:51 +0700

    fix loi

--- web/next.config.ts
+++ web/next.config.ts
@@ -4,7 +4,7 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
-  cacheComponents: true,
+
   images: {
     remotePatterns: [
       {
@@ -18,9 +18,22 @@ const nextConfig: NextConfig = {
       'i.pinimg.com',
       'lh3.googleusercontent.com',
       'scontent.fsgn5-9.fna.fbcdn.net',
-      'https://hdcamp.id.vn'
-
+      'hdcamp.id.vn'
     ],
+  },
+
+  async headers() {
+    return [
+      {
+        source: '/(.*)',
+        headers: [
+          {
+            key: 'Cross-Origin-Opener-Policy',
+            value: 'same-origin-allow-popups',
+          },
+        ],
+      },
+    ];
   },
 };
 

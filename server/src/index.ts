5a92e78e5e40e541e67f56b847cb13f6a4b625ad
vo phu hao <vophuhao2004@gmail.com>
2026-06-02 18:03:58 +0700

    f

--- server/src/index.ts
+++ server/src/index.ts
@@ -80,8 +80,11 @@ app.use(express.json({ limit: "10mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 app.use(
   cors({
-    // origin: APP_ORIGIN,
-    origin: "https://www.hdcamp.id.vn",
+    origin: [
+      APP_ORIGIN,
+      "https://www.hdcamp.id.vn",
+
+    ],
     credentials: true,
   })
 );
